64ad67485a6ded1caa290d2e711649e6e11bf5a7
shinjo_tsuyoshi <shinjo.tsuyoshi.st@gmail.com>
2023-03-08 15:37:59 +0900

    test

---
+++ 水本さんeclipse/Ans/Java1.5_ans/src/variable/Variable1Ans.java
@@ -0,0 +1,30 @@
+package variable;
+
+public class Variable1Ans {
+	public static void main(String[] args) {
+//		問１）
+//		コンソールに以下のような自己紹介を自分の情報で出力してください。
+//		※但し、「名前、性別、年齢、身長」は変数に代入して、出力するようにしてください。
+//		※性別は「男」か「女」の一文字、身長は小数点第一位までのかたちで表示されるようにして下さい。
+//		
+//		例）
+//		私の自己紹介をします。
+//		名前は山田太郎です。
+//		性別は男です。
+//		年齢は19歳です。
+//		身長は174.2㎝です。
+//		よろしくお願いします。
+		System.out.println("私の自己紹介をします。");
+
+		String name = "モンキー・D・ルフィ";
+		char gender = '男';
+		int age = 19;
+		double height = 174.2;
+
+		System.out.println("名前は" + name + "です。");
+		System.out.println("性別は" + gender + "です。");
+		System.out.println("年齢は" + age + "歳です。");
+		System.out.println("身長は" + height + "㎝です。");
+		System.out.println("よろしくお願いします。");
+	}
+}
---
+++ 水本さんeclipse/Java1.8/src/bool/Bool.java
@@ -0,0 +1,29 @@
+package bool;
+
+public class Bool {
+	public static void main(String[] args) {
+//		問１）
+//		以下の変数を使ってboolean型変数 result_1 に「変数 a の値は変数 b の値以上」という条件式を代入して下さい。
+//		boolean型変数 result_1 をコンソールに出力して下さい。
+		int a = 20;
+		int b = 5;
+
+//		問２）
+//		以下の変数を使ってboolean型変数 result_2 に「変数 c の値と変数 d の値は等しくない」という条件式を代入して下さい。
+//		boolean型変数 result_2 をコンソールに出力して下さい。
+		int c = 5;
+		int d = 10;
+		
+//		問３）
+//		以下の変数を使ってboolean型変数 result_3 に「変数 e の値は10より大きい かつ 変数 f の値は10未満」という条件式を代入して下さい。
+//		boolean型変数 result_3 をコンソールに出力して下さい。
+		int e = 12;
+		int f = 8;
+		
+//		問４）
+//		以下の変数を使ってboolean型変数 result_4 に「変数 g の値は5以下 または 変数 g の値は10以上」という条件式を代入して下さい。
+//		boolean型変数 result_4 をコンソールに出力して下さい。
+		int g = 7;
+
+	}
+}
